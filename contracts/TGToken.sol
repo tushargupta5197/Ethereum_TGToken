83a338a7dbc55c8daba76ad158943eddaf982ffe
Tushar Gupta <tushargupta5197@gmail.com>
2018-12-17 13:37:30 +0000

    transfer function according to erc20 standard

--- contracts/TGToken.sol
+++ contracts/TGToken.sol
@@ -9,6 +9,13 @@ contract TGToken {
 	// Standard
 	string public standard = "TG Token v1.0";
 
+	//transfer event
+	event Transfer(
+		address indexed _from,
+		address indexed _to,
+		uint256 _value
+	);
+
 	// Constructor
 	// Set the total number of tokens
 	// Read the total number of tokens
@@ -20,5 +27,20 @@ contract TGToken {
 	constructor (uint256 _initialSupply) public {
 		balanceOf[msg.sender] = _initialSupply;
 		totalSupply = _initialSupply;
+	}
+
+	// Transfer
+
+	function transfer(address _to, uint256 _value) public returns (bool success) {
+		// exception if does not have enough
+		require(balanceOf[msg.sender] >= _value);
+		// transfer the balance
+		balanceOf[msg.sender] -= _value;
+		balanceOf[_to] += _value;
+		// transfer event
+		Transfer(msg.sender, _to, _value);
+		// return boolean
+		return true;
+		
 	}
 }
--- test/TGToken.js
+++ test/TGToken.js
@@ -30,4 +30,37 @@ contract('TGToken', function(accounts){
 		});
 
 	});
+
+	it('transfers ownership', function(){
+		return TGToken.deployed().then(function(instance){
+			tokenInstance = instance;
+			// testing require, sending more tokens
+			return tokenInstance.transfer.call(accounts[1], 999999999);
+		}).then(assert.fail).catch(function(error){
+			assert(error.message.indexOf('revert') >= 0, 'error message contains revert');
+			return tokenInstance.transfer.call(accounts[1], 250000, {from: accounts[0]});
+		}).then(function(success){
+			assert.equal(success, true, 'it returns true');
+			return tokenInstance.transfer(accounts[1], 250000, {from: accounts[0]});
+		}).then(function(receipt){
+			assert.equal(receipt.logs.length, 1, 'triggers one event');
+			assert.equal(receipt.logs[0].event, 'Transfer', 'should be the Transfer event');
+			assert.equal(receipt.logs[0].args._from, accounts[0], 'check sender');
+			assert.equal(receipt.logs[0].args._to, accounts[1], 'check receiver');
+			assert.equal(receipt.logs[0].args._value, 250000, 'logs the amount');
+			return tokenInstance.balanceOf(accounts[1]);
+		}).then(function(balance){
+			assert.equal(balance.toNumber(), 250000, 'adds the amount to receiving account');
+			return tokenInstance.balanceOf(accounts[0]);
+		}).then(function(balance){
+			assert.equal(balance.toNumber(), 750000, 'deducts from the receiving amount');
+		});
+	});
+
+
+
+
+
+
+
 });
